bfc617378c4d2acb8b5c9e4cad0eab3dc449cf60
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-03-01 00:37:20 +0530

    Create Introduction to Currying and Partial Application.js

---
+++ Introduction to Currying and Partial Application.js
@@ -0,0 +1,17 @@
+function add(x) {
+  // Add your code below this line
+  return function(y) {
+    return function(z) {
+      return x + y + z;
+    };
+  };
+  // Add your code above this line
+}
+add(10)(20)(30);
+
+function add(x) {
+  // Add your code below this line
+  return y => z => x + y + z;
+  // Add your code above this line
+}
+add(10)(20)(30);
